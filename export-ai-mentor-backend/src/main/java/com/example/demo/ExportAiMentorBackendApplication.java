0b1104aa308fc7bb90d56104418f40fd4122313e
Akshad-Mali1603 <akshadmali16@gmail.com>
2025-07-31 02:40:54 +0530

    First Backend Commit

---
+++ export-ai-mentor-backend/src/main/java/com/example/demo/ExportAiMentorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportAiMentorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportAiMentorBackendApplication.class, args);
+	}
+
+}
