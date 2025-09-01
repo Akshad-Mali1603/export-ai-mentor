c7dc310bbc04e9185a8878c40397b1c71b5d55ff
Saloni Tomar <tomarsaloni19@gmail.com>
2025-09-01 17:26:03 +0530

    Modify aboutUs Page

--- export-ai-mentor-frontend/src/Domain/Pages/aboutUs.tsx
+++ export-ai-mentor-frontend/src/Domain/Pages/aboutUs.tsx
@@ -1,11 +1,15 @@
 import OurMission from "../AboutUs/OurMission";
 import Header from "../Header/Header";
 import Footer from "../LandingPage/Footer";
+import OurStory from "../AboutUs/OurStory";
+import OurVisions from "../AboutUs/OurVisions";
 
 const AboutUs = () => {
     return <div><div className="min-h-[100vh] bg-slate-100 font-['poppins']">
         <Header />
        <OurMission />
+       <OurStory />
+       <OurVisions />
         <Footer />
     </div>
     </div>;
