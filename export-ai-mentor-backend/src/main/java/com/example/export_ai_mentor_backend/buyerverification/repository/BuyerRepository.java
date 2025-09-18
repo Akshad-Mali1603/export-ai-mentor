d149e94a79c5c3af03b4da9c29a5e0975fc0f674
Akshad-Mali1603 <akshadmali16@gmail.com>
2025-09-18 12:04:20 +0530

    a.text file added

--- export-ai-mentor-backend/src/main/java/com/example/export_ai_mentor_backend/buyerverification/repository/BuyerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.export_ai_mentor_backend.buyerverification.repository;
-
-import com.example.export_ai_mentor_backend.buyerverification.entity.BuyerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuyerRepository extends JpaRepository<BuyerEntity, Long> {
-
-}
